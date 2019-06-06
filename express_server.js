5dede357cca6861b2ceff4d8e676a8657e778e43
simongarber <simon.garber@gmail.com>
2019-06-06 00:44:41 +0000

    created the URL submission form, added a post route to log the form submission to the console and installed the body parser that converts the data into a string implemented the random string generator function

--- express_server.js
+++ express_server.js
@@ -1,3 +1,6 @@
+function generateRandomString() {
+
+}
 
 var express = require("express");
 var app = express();
@@ -8,12 +11,27 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
   console.log(templateVars)
   res.render("urls_show", templateVars);
 
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+
+});
+
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -35,4 +53,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
