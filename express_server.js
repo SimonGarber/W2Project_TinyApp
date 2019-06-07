79ab802e3f63fb3977597b3f18875a48c1663cfd
simongarber <simon.garber@gmail.com>
2019-06-07 17:49:57 +0000

    fixed the login form by implementing the req.cookie information to include the username.Added the header information to each of the ejs files so that a user that has logged in will not be asked to log in again while the cookie is present.

--- express_server.js
+++ express_server.js
@@ -10,11 +10,11 @@ for ( var i = 0; i < 6; i++ ) {
    return result;
 };
 
-
 var express = require("express");
+var cookieParser = require('cookie-parser');
 var app = express();
 var PORT = 8080; // default port 8080
-
+app.use(cookieParser());
 app.set("view engine", "ejs")
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,27 +25,37 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new",{
+    username:req.cookies.username
+  });
 
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], 
+  username: req.cookies.username
+  };
   // console.log(templateVars)
   res.render("urls_show", templateVars);
 
 });
 
+// app.get("/urls/login",(req,res) =>
+
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase, 
+    username:req.cookies.username
+  };
   res.render("urls_index", templateVars);
 
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/",(req, res) => {
+ console.log('Cookies: ', req.cookies)
+ res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
@@ -86,5 +96,14 @@ res.redirect("/urls");
 
 });
 
-
+app.post("/login",(req,res) =>{
+  /**
+   * Reaction from the site after the user logs their login information and presses the button.
+   * the site will log a cookie and redirect them to the urls page.
+   * 
+   */
+  const username = req.body.username
+  res.cookie("username",username)
+  res.redirect("/"); 
+});
 
